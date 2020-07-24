dcb0f145286006059032d9624fe73302393cb69d
y24er <1028704486@qq.com>
2020-07-24 15:35:54 +0800

    [ParkingLot][Jessie] feature: implement should fetch car then fetch car given car ticket

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,7 +1,18 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    Map<CarTicket, Car> packingRooms = new HashMap<>();
+
     public CarTicket park(Car car) {
-        return new CarTicket();
+        CarTicket carTicket = new CarTicket();
+        packingRooms.put(carTicket, car);
+        return carTicket;
+    }
+
+    public Car fetch(CarTicket carTicket) {
+        return packingRooms.get(carTicket);
     }
 }
