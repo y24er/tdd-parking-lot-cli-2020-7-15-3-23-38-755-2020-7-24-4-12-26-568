d225345e1e37f51a41488c83a10598ef9c416050
y24er <1028704486@qq.com>
2020-07-24 16:27:08 +0800

    [ParkingLot][Jessie] feature: implement should_return_no_car_ticket_and_not_park_in_when_park_from_full_capacity_parking_lot_given_car

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
+    private final int capacity = 10;
     Map<CarTicket, Car> packingRooms = new HashMap<>();
 
     public CarTicket park(Car car) {
         CarTicket carTicket = new CarTicket();
-        packingRooms.put(carTicket, car);
-        return carTicket;
+        if (packingRooms.size() != capacity)
+            packingRooms.put(carTicket, car);
+        return packingRooms.size() != capacity ? carTicket : null;
     }
 
     public Car fetch(CarTicket carTicket) {
