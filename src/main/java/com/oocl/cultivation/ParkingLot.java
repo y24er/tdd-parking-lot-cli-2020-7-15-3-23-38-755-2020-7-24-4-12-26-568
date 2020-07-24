0c15b851220144921f61676287ae361429c3fac5
y24er <1028704486@qq.com>
2020-07-24 15:24:53 +0800

    [ParkingLot][Jessie] feature: implement should return car ticket then park given car test case

---
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -0,0 +1,7 @@
+package com.oocl.cultivation;
+
+public class ParkingLot {
+    public CarTicket park(Car car) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -0,0 +1,23 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkingLot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+    @Test
+    void should_return_car_ticket_then_park_given_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+
+        //when
+        CarTicket carTicket = parkingLot.park(car);
+
+        //then
+        assertNotNull(carTicket);
+    }
+}
