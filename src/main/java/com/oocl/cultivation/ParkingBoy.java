73f2d8b7005fcf29cad67169416bd5ffa265a187
y24er <1028704486@qq.com>
2020-07-25 17:24:26 +0800

    [Story1][Jessie] feature: implement should_return_Car_ticket_when_park_given_car test case

---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,18 @@
+package com.oocl.cultivation;
+
+public class ParkingBoy {
+    ParkingLot parkingLot = new ParkingLot();
+
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public CarTicket park(Car car) {
+        return null;
+    }
+
+
+    /*public CarTicket park(Car car) {
+
+    }*/
+}
