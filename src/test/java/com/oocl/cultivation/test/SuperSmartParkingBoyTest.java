0de46bb7d7164f37244da5d2f4de0c9f0540aa97
y24er <1028704486@qq.com>
2020-07-26 12:58:47 +0800

    [Story5][Jessie] feature: implement should_return_car_ticket_when_park_car_in_larger_available_position_rate_parking_lot_given_car test case

--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -1,4 +1,48 @@
 package com.oocl.cultivation.test;
 
-public class SuperSmartParkingBoy {
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.SuperSmartParkingBoy;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SuperSmartParkingBoyTest {
+    @Test
+    void should_return_car_ticket_when_park_car_in_larger_available_position_rate_parking_lot_given_car() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot(1, 5);
+        ParkingLot parkingLot2 = new ParkingLot(2, 6);
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLot1, parkingLot2);
+
+        //when
+        Car car1 = new Car();
+        Object result1 = superSmartParkingBoy.park(car1);
+        CarTicket carTicket1 = null;
+        if (result1 instanceof CarTicket)
+            carTicket1 = (CarTicket) result1;
+
+        Car car2 = new Car();
+        Object result2 = superSmartParkingBoy.park(car2);
+        CarTicket carTicket2 = null;
+        if (result2 instanceof CarTicket)
+            carTicket2 = (CarTicket) result2;
+
+        Car car3 = new Car();
+        Object result3 = superSmartParkingBoy.park(car3);
+        CarTicket carTicket3 = null;
+        if (result3 instanceof CarTicket)
+            carTicket3 = (CarTicket) result3;
+
+        //then
+        assertNotNull(carTicket1);
+        assertEquals(1, carTicket1.getParkingLotId());
+        assertNotNull(carTicket2);
+        assertEquals(2, carTicket2.getParkingLotId());
+        assertNotNull(carTicket3);
+        assertEquals(2, carTicket3.getParkingLotId());
+    }
+
 }
