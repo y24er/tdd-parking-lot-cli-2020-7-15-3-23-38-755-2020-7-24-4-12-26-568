170f8d1d3e9c402d8d02be4d587b642edea150de
y24er <1028704486@qq.com>
2020-07-26 12:11:03 +0800

    [Story4][Jessie] test: implement     should_return_not_enough_position_when_park_car_to_all_full_capacity_parking_lot_given_car

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -13,7 +13,9 @@ public class SmartParkingBoyTest {
     @Test
     void should_return_car_ticket_when_park_car_from_more_empty_positions_given_car() {
         //given
-        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot(1, 5), new ParkingLot(2, 5));
+        ParkingLot parkingLot1 = new ParkingLot(1, 5);
+        ParkingLot parkingLot2 = new ParkingLot(2, 5);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot1, parkingLot2);
         Car car1 = new Car();
         Car car2 = new Car();
 
@@ -24,6 +26,24 @@ public class SmartParkingBoyTest {
         //then
         assertEquals(1, carTicket1.getParkingLotId());
         assertEquals(2, carTicket2.getParkingLotId());
+    }
+
+    @Test
+    void should__when_park_car_from_more_empty_positions_given_car() {
+        //given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot(1, 1), new ParkingLot(2, 1));
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        //when
+        CarTicket carTicket1 = (CarTicket) smartParkingBoy.park(car1);
+        CarTicket carTicket2 = (CarTicket) smartParkingBoy.park(car2);
+        String result = (String) smartParkingBoy.park(car3);
+
+        //then
+        assertEquals(1, carTicket1.getParkingLotId());
+        assertEquals(2, carTicket2.getParkingLotId());
+        assertEquals("Not enough position.", result);
     }
 
 }
---
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -0,0 +1,4 @@
+package com.oocl.cultivation.test;
+
+public class SuperSmartParkingBoy {
+}
