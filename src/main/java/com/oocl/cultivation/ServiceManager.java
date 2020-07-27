7877e20e779a9f4830c41bf270d523e9c5c12986
y24er <1028704486@qq.com>
2020-07-27 22:19:18 +0800

    [ParkingLot][Jessie] refactor: delete the ServiceManager and ServiceManagerTest content

--- src/main/java/com/oocl/cultivation/ServiceManager.java
+++ src/main/java/com/oocl/cultivation/ServiceManager.java
@@ -1,15 +1,5 @@
 package com.oocl.cultivation;
 
-import java.util.List;
-
 public class ServiceManager {
-    ParkingLot parkingLot;
 
-    public ServiceManager(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
-    }
-
-    public List addParkingBoy(ParkingBoy parkingBoy) {
-        return null;
-    }
 }
--- src/test/java/com/oocl/cultivation/test/ServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/test/ServiceManagerTest.java
@@ -1,24 +1,16 @@
 package com.oocl.cultivation.test;
 
-import com.oocl.cultivation.ParkingBoy;
-import com.oocl.cultivation.ParkingLot;
-import com.oocl.cultivation.ServiceManager;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 public class ServiceManagerTest {
     @Test
     void should_parking_boy_list_has_the_added_boy_when_add_parking_boy_to_management_list_given_parking_boy() {
         //given
-        ParkingLot parkingLot = new ParkingLot();
-        ServiceManager serviceManager = new ServiceManager(parkingLot);
-        ParkingBoy parkingBoy = new ParkingBoy();
+
         //when
-        List parkingBoyList = serviceManager.addParkingBoy(parkingBoy);
 
         //then
-        assertThat(parkingBoyList,hasItem(parkingBoy));
+
     }
 
 }
