4d7f395f7b9b147d635f25ed8483d027f4e59763
y24er <1028704486@qq.com>
2020-07-27 22:25:28 +0800

    [ParkingLot][Jessie] refactor: add test method to smart packing boy

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -1,12 +1,12 @@
 package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
 import com.oocl.cultivation.ParkingLot;
 import com.oocl.cultivation.SmartParkingBoy;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartParkingBoyTest {
 
@@ -26,6 +26,73 @@ public class SmartParkingBoyTest {
         //then
         assertEquals(1, parkingLot1.getPackingRooms().size());
         assertEquals(1, parkingLot2.getPackingRooms().size());
+    }
+
+    @Test
+    void should_return_right_car_when_fetch_car_given_car_ticket() {
+        //given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot());
+        Car car = new Car();
+        CarTicket carTicket = smartParkingBoy.park(car);
+
+        //when
+        Car fetchCar = smartParkingBoy.fetch(carTicket);
+
+        //then
+        assertNotNull(carTicket);
+        assertNotNull(fetchCar);
+    }
+
+    @Test
+    void should_not_fetch_car_and_return_unrecognized_parking_ticket_when_fetch_car_given_wrong_ticket() {
+        //given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot());
+        CarTicket carTicket = smartParkingBoy.park(new Car());
+        CarTicket wrongCarTicket = new CarTicket();
+
+        //when
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            smartParkingBoy.fetch(wrongCarTicket);
+        });
+
+        //then
+        assertNotNull(carTicket);
+        assertNotEquals(carTicket, wrongCarTicket);
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
+
+    @Test
+    void should_not_fetch_car_and_return_unrecognized_parking_ticket_when_fetch_car_given_has_been_used_ticket() {
+        //given
+        Car car = new Car();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot());
+        CarTicket carTicket = smartParkingBoy.park(car);
+        CarTicket wrongCarTicket = new CarTicket();
+        Car fetchCar = smartParkingBoy.fetch(carTicket);
+
+        //when
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            smartParkingBoy.fetch(carTicket);
+        });
+
+        //then
+        assertNotNull(carTicket);
+        assertEquals(car, fetchCar);
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
+
+    @Test
+    void should_not_fetch_car_and_return_please_provide_your_parking_ticket_ticket_when_fetch_car_given_null_car_ticket() {
+        //given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot());
+
+        //when
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            smartParkingBoy.fetch(null);
+        });
+
+        //then
+        assertEquals("Please provide your parking ticket.", exception.getMessage());
     }
 
     @Test
